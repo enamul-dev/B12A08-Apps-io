5e09fe99cbfae19afc3d13648e18f3a761691b08
enamul <enamul.dev01@gmail.com>
2026-04-18 07:47:13 +0600

    icons fixed

--- src/App.jsx
+++ src/App.jsx
@@ -38,8 +38,17 @@ function App() {
           <p className='text-center text-gray-500'>At HERO.IO we craft innovative apps designed to make everyday life simpler, smarter, and more exciting. <br /> Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
           <br />
           <div className='flex gap-2 justify-center'>
-            <button className='btn bg-transparent border-1'><i class="fa-brands fa-google-play"></i> <span><img src="" alt="" /></span> Google Play</button>
-            <button className='btn bg-transparent border-1'><i class="fa-brands fa-app-store-ios"></i> App Store</button>
+            <button href="https://www.w3schools.com/" target="_blank" className='btn bg-transparent ' > <svg className='w-6 h-6' xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd"
+viewBox="0 0 466 511.98">
+ <g id="Layer_x0020_1">
+  <path fill="#EA4335" fill-rule="nonzero" d="M199.9 237.8l-198.5 232.37c7.22,24.57 30.16,41.81 55.8,41.81 11.16,0 20.93,-2.79 29.3,-8.37l0 0 244.16 -139.46 -130.76 -126.35z"/>
+  <path fill="#FBBC04" fill-rule="nonzero" d="M433.91 205.1l0 0 -104.65 -60 -111.61 110.22 113.01 108.83 104.64 -58.6c18.14,-9.77 30.7,-29.3 30.7,-50.23 -1.4,-20.93 -13.95,-40.46 -32.09,-50.22z"/>
+  <path fill="#34A853" fill-rule="nonzero" d="M199.42 273.45l129.85 -128.35 -241.37 -136.73c-8.37,-5.58 -19.54,-8.37 -30.7,-8.37 -26.5,0 -50.22,18.14 -55.8,41.86 0,0 0,0 0,0l198.02 231.59z"/>
+  <path fill="#4285F4" fill-rule="nonzero" d="M1.39 41.86c-1.39,4.18 -1.39,9.77 -1.39,15.34l0 397.64c0,5.57 0,9.76 1.4,15.34l216.27 -214.86 -216.28 -213.46z"/>
+ </g>
+</svg>
+ <a href="https://play.google.com/store/games?hl=en" target="_blank" className='text-black'>Google Play</a></button>
+            <button className='btn bg-transparent'><img src="appstore.svg" className='w-10 h-10' alt="" /><a href="https://www.apple.com/app-store/" target="_blank" className='text-black'>App Store</a></button>
           </div>
           <br />
           <div>
@@ -49,10 +58,11 @@ function App() {
       </section>
 
 
-    
+    {/* Json Data section */}
 
 <section className="py-10 bg-gray-100 ">
-  <h1 className="text-3xl font-bold text-center mb-8">Our Apps</h1>
+  <h1 className="text-3xl font-bold text-center mb-5 text-black">Trending Apps</h1>
+  <p className='text-center text-gray-400 pb-10'>Explore All Trending Apps on the Market developed by us</p>
   <div className="grid grid-cols-1 md:grid-cols-4 gap-6 px-6 container">
     {appData.slice(0, 8).map(app => (
       <div key={app.id} className="bg-white shadow-md rounded-lg p-5 text-center">
